175e7f4fc494401334a600605ec5023e71551bc9
Alex <alexlistopad@mail.ru>
2023-01-22 21:21:05 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -38,4 +38,50 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+
+    // timer
+    let deadline = '2023-01-10';
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date());  // отнимаем от конечной даты текущую в милисекундах
+        let seconds = Math.floor((t / 1000) % 60);         // Math.floor - округляем до целого числа, t / 1000 - делаем из милисекунд - секунды, %60 - остаток от дедения на 60 секунд
+        let minutes = Math.floor((t / 1000 / 60 % 60));    // получаем кол-во часов и остаток в минутах
+        let hours = Math.floor((t / 1000 / 60 / 60) % 24);    // часы
+        let days = Math.floor((t / (1000 * 60 * 60 * 24)));    // дни
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            days.textContent = t.days;
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+    setClock('timer', deadline);
 }); 
